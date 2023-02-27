e273a97dbb30e7e8afb711f4a3dafa231b86261d
Ryan Weald <ryan@weald.com>
2023-02-26 19:31:56 -0800

    Setup app navigation using react-navigation

--- App.tsx
+++ App.tsx
@@ -5,12 +5,39 @@
  * @format
  */
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import AppAccountSignupScreen from './src/screens/AppAccountSignupScreen';
+import ClaimScreen from './src/screens/ClaimScreen';
 
 import LandingScreen from './src/screens/LandingScreen';
+import LogoScreen from './src/screens/LogoScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import SeedPhraseScreen from './src/screens/SeedPhraseScreen';
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
-  return <LandingScreen />;
+  // return <LandingScreen />;
+  // return <AppAccountSignupScreen />;
+  // return <ClaimScreen />;
+  // return <LogoScreen />;
+  // return <ProfileScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Landing"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Landing" component={LandingScreen} />
+        <Stack.Screen name="Signup" component={AppAccountSignupScreen} />
+        <Stack.Screen name="Claim" component={ClaimScreen} />
+        <Stack.Screen name="Home" component={LogoScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Seed" component={SeedPhraseScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
