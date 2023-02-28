9361a83fe07667cea3af38c25165b7bfd564a922
Ryan Weald <ryan@weald.com>
2023-02-27 22:01:13 -0800

    Pull real account details onto profile screen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -2,12 +2,30 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {Image, Text, View} from 'react-native';
+import {useRecoilValue} from 'recoil';
 import ScreenContainer from '../components/ScreenContainer';
 import StandardButton from '../components/StandardButton';
 import {StandardHeader} from '../components/StandardHeader';
+import {account} from '../state';
 
 export default function ProfileScreen() {
   const navigation = useNavigation();
+  const rlyAccount = useRecoilValue(account);
+
+  const acountHumanReadable = () => {
+    if (!rlyAccount) {
+      return '';
+    }
+
+    const firstChars = rlyAccount.slice(0, 5);
+    const lastChars = rlyAccount.slice(
+      rlyAccount.length - 3,
+      rlyAccount.length,
+    );
+
+    return `${firstChars}...${lastChars}`;
+  };
+
   return (
     <>
       <StandardHeader />
@@ -20,7 +38,9 @@ export default function ProfileScreen() {
           <Text style={{fontSize: 20, fontWeight: 'bold', marginTop: 16}}>
             @username
           </Text>
-          <Text style={{fontSize: 16, marginTop: 8}}>OxFoo...bar</Text>
+          <Text style={{fontSize: 16, marginTop: 8}}>
+            {acountHumanReadable()}
+          </Text>
           <View style={{marginTop: 48}}>
             <StandardButton title="Copy address" onPress={async () => {}} />
           </View>
