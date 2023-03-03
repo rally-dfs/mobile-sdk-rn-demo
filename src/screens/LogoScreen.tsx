89a5dc5a0ce5ed9e4c981975fab3b8f7f8167102
Ryan Weald <ryan@weald.com>
2023-03-02 16:15:43 -0800

    Fix flash on logo home screen as async balance gets pulled

--- src/screens/LogoScreen.tsx
+++ src/screens/LogoScreen.tsx
@@ -39,39 +39,48 @@ export default function LogoScreen() {
   return (
     <>
       <StandardHeader />
-      <ScreenContainer>
-        <View
-          style={{
-            marginTop: 96,
-            alignItems: 'center',
-          }}>
-          <Image
-            source={logoColor()}
-            style={{height: 243, width: 243, alignSelf: 'center'}}
-          />
-        </View>
-        <View
-          style={{
-            marginTop: 116,
-            alignItems: 'center',
-            alignSelf: 'center',
-          }}>
-          <Text style={{fontSize: 16}}>Upgrade your RLY Logo color!</Text>
-          <View style={{marginTop: 24}}>
-            <StandardButton
-              title={transfering ? 'Sending...' : 'Use 1 RLY'}
-              disabled={!balance}
-              onPress={sendRly}
+      {balance === undefined ? (
+        <ScreenContainer>
+          <View
+            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <ActivityIndicator />
+          </View>
+        </ScreenContainer>
+      ) : (
+        <ScreenContainer>
+          <View
+            style={{
+              marginTop: 96,
+              alignItems: 'center',
+            }}>
+            <Image
+              source={logoColor()}
+              style={{height: 243, width: 243, alignSelf: 'center'}}
             />
           </View>
-
-          {transfering && (
-            <View style={{marginTop: 12}}>
-              <ActivityIndicator />
+          <View
+            style={{
+              marginTop: 116,
+              alignItems: 'center',
+              alignSelf: 'center',
+            }}>
+            <Text style={{fontSize: 16}}>Upgrade your RLY Logo color!</Text>
+            <View style={{marginTop: 24}}>
+              <StandardButton
+                title={transfering ? 'Sending...' : 'Use 1 RLY'}
+                disabled={!balance}
+                onPress={sendRly}
+              />
             </View>
-          )}
-        </View>
-      </ScreenContainer>
+
+            {transfering && (
+              <View style={{marginTop: 12}}>
+                <ActivityIndicator />
+              </View>
+            )}
+          </View>
+        </ScreenContainer>
+      )}
     </>
   );
 }
