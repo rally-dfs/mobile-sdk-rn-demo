a4449f728a69cf1f63320b5130e7cfd35f64b818
Ryan Weald <ryan@weald.com>
2023-06-12 17:09:35 -0700

    Point app back at Mumbai

--- App.tsx
+++ App.tsx
@@ -8,12 +8,12 @@
 import React from 'react';
 
 import {RecoilRoot} from 'recoil';
-import {RlyLocalNetwork, Network} from '@rly-network/mobile-sdk';
+import {RlyMumbaiNetwork, Network} from '@rly-network/mobile-sdk';
 
 import AppRouting from './src/components/AppRouting';
 import ErrorToast from './src/components/ErrorToast';
 
-export const RlyNetwork: Network = RlyLocalNetwork;
+export const RlyNetwork: Network = RlyMumbaiNetwork;
 
 function App(): JSX.Element {
   return (
