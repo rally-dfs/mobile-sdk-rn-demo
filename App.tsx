8605e9d3bf2bdb0641dd0503a554ebd4ff6403e6
Ryan Weald <ryan@weald.com>
2023-07-05 15:44:02 -0700

    Setup app with API key for hitting hosted gassless infra

--- App.tsx
+++ App.tsx
@@ -12,8 +12,10 @@ import {RlyMumbaiNetwork, Network} from '@rly-network/mobile-sdk';
 
 import AppRouting from './src/components/AppRouting';
 import ErrorToast from './src/components/ErrorToast';
+import {PrivateConfig} from './src/private_config';
 
 export const RlyNetwork: Network = RlyMumbaiNetwork;
+RlyNetwork.setApiKey(PrivateConfig.RALLY_API_KEY);
 
 function App(): JSX.Element {
   return (
